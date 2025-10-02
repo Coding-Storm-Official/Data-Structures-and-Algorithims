dd8fdf0bba8a0e0542dc44de645d9047dc0a7cc9
Coding-Storm-Official <96690322+Coding-Storm-Official@users.noreply.github.com>
2025-10-03 03:03:07 +0530

    Update Greatest Common Divisor (Euclidean)

--- Greatest Common Divisor (Euclidean).py
+++ Greatest Common Divisor (Euclidean).py
@@ -1,4 +1,4 @@
-#My approach - O(min(a,b)) [Brute-force]
+#My approach - O(min(a,b)) [Brute-force] 
 a=int(input("A: "))
 b=int(input("B: "))
 fc = max(a,b)
@@ -22,3 +22,4 @@ b = int(input())
 while b:
     a, b = b, a % b
 print(a)
+
