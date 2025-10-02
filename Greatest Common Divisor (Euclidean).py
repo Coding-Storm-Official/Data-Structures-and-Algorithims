eccc0cb942b0a8fa7b5dbc52119a5af961ad6e6f
Coding-Storm-Official <96690322+Coding-Storm-Official@users.noreply.github.com>
2025-10-03 02:43:39 +0530

    Add files via upload
    First approach is the one I originally had which was brute-force that was incredibly slow thus making it inefficient to use. Brute-force has a time complexity of O(min(a,b)) 😭.
    Second approach is using the Euclidean algorithm with a time complexity of O(log(min(a,b))) in which what I basically did was run a while loop till our variable is 0 and then swap the first variable's value with the second variable's value and replace the second variable's value with the divisor we get at that specific iteration and so in the end we will end up with the greatest divisor!

---
+++ Greatest Common Divisor (Euclidean).py
@@ -0,0 +1,18 @@
+#My approach - O(min(a,b)) [Brute-force]
+a=int(input("A: "))
+b=int(input("B: "))
+fc = max(a,b)
+fcd = []
+ans = 1
+for i in range(1,fc):
+    if fc%i==0 and i<=b:
+        fcd.append(i)
+
+for i in range(len(fcd)):
+    if b%fcd[i]==0:
+        ans = max(ans,fcd[i])
+
+print(ans)
+
+
+#
